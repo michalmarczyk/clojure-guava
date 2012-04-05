b86bac6004562136ba97d094a0b65086f2a65ddc
Michał Marczyk <michal.marczyk@gmail.com>
2012-04-05 21:32:48 +0200

    0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-guava "0.0.2-SNAPSHOT"
+(defproject clojure-guava "0.0.2"
   :description "Clojure wrappers around the immutable Guava collections"
   :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
                  [com.google.guava/guava "10.0.1"]])
