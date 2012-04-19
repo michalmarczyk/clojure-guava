99092b20e61c08d160b1d41d5d1a0f4a508197e5
Michał Marczyk <michal.marczyk@gmail.com>
2012-04-19 14:59:56 +0200

    Add criterium to :dev-dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-guava "0.0.7-SNAPSHOT"
   :description "Clojure wrappers around the immutable Guava collections"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
-                 [com.google.guava/guava "10.0.1"]])
+  :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]
+                 [com.google.guava/guava "10.0.1"]]
+  :dev-dependencies [[criterium "0.2.0"]])
