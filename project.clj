af7d0a0dfd0f71d0337128bc7c9ff31105287622
Michał Marczyk <michal.marczyk@gmail.com>
2012-04-24 15:55:45 +0200

    Use criterium-0.2.1
    Also bumps version to 0.0.8-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojure-guava "0.0.7"
+(defproject clojure-guava "0.0.8-SNAPSHOT"
   :description "Clojure wrappers around the immutable Guava collections"
   :dependencies [[org.clojure/clojure "[1.2.0,1.4.0]"]
                  [com.google.guava/guava "10.0.1"]]
-  :dev-dependencies [[criterium "0.2.0"]])
+  :dev-dependencies [[criterium "0.2.1"]])
